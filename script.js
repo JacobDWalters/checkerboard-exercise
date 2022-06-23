e61d5f404d73b668f7e13786e4eecea79a622b54
JacobDWalters <99148047+JacobDWalters@users.noreply.github.com>
2022-06-23 16:32:39 -0600

    Checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Your JS goes here
+var container = document.createElement('div');
+document.body.append(container);
+counter = 0
+
+for (var i = 0; i < 81; i++) {
+    //i %2 === 0 ? div.style.color = 'black';
+    var div = document.createElement('div');
+    container.append(div);
+    counter ++;
+    div.style.width = '10.9%'
+    div.style.float = 'left'
+    //div.style.background = 'red'
+    div.style.paddingBottom = '11.1%'
+    div.style.border = '1px solid black'
+    div.id = counter;
+    div.id %2 === 0 ? 
+        div.style.background = 'black' : 
+        div.style.background = 'red'
+}
+
